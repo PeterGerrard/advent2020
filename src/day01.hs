ca008f47cf13d3579c31c8f770a1c4cc828bc61a
Peter Gerrard <peter.gerrard@red-gate.com>
2020-12-02 13:18:26 +0000

    Day 1

--- src/day01.hs
+++ src/day01.hs
@@ -1,1 +1,5 @@
-main = interact id
+solve1 xs = [x * y | x <- xs, y <- xs, x <= y, x + y == 2020]
+
+solve2 xs = [x * y * z | x <- xs, y <- xs, z <- xs, x <= y, y <= z, x + y + z == 2020]
+
+main = interact $ unlines . map show . solve2 . map read . lines
